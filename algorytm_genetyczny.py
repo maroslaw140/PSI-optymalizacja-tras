238c12a72e9d1dc4f750046b683cf7f7ff339e86
maroslaw140 <marek.muller911@gmail.com>
2024-01-09 23:34:59 +0100

    Proces ewolucji

---
+++ algorytm_genetyczny.py
@@ -0,0 +1,28 @@
+import random
+
+def generuj_populacje(tablica, wielkosc_populacji):
+    poczatkowa_populacja = []
+    for _ in range(wielkosc_populacji):
+        chromosom = random.sample(tablica, len(tablica))
+        poczatkowa_populacja.append(chromosom)
+    return poczatkowa_populacja
+
+def selekcja_turniejowa(populacja, wyniki_oceny, rozmiar_turnieju):
+    nowa_populacja = []
+    for _ in range(len(populacja)):
+        turniej = random.sample(list(enumerate(wyniki_oceny)), rozmiar_turnieju)
+        zwyciezca_turnieju = min(turniej, key=lambda x: x[1])[0]
+        nowa_populacja.append(populacja[zwyciezca_turnieju])
+    return nowa_populacja
+
+def krzyzowanie_jednopunktowe(rodzic1, rodzic2):
+    punkt_podzialu = random.randint(0, len(rodzic1) - 1)
+    dziecko1 = rodzic1[:punkt_podzialu] + [gen for gen in rodzic2 if gen not in rodzic1[:punkt_podzialu]]
+    dziecko2 = rodzic2[:punkt_podzialu] + [gen for gen in rodzic1 if gen not in rodzic2[:punkt_podzialu]]
+    return dziecko1, dziecko2
+
+def mutacja_zmiany_miast(chromosom, prawdopodobienstwo_mutacji):
+    if random.random() < prawdopodobienstwo_mutacji:
+        indeks1, indeks2 = random.sample(range(len(chromosom)), 2)
+        chromosom[indeks1], chromosom[indeks2] = chromosom[indeks2], chromosom[indeks1]
+    return chromosom
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from classAlgorytmGenetyczny import AlgorytmGenetyczny
 from classKlient import Klient
 from classKoszt import Koszt
+from algorytm_genetyczny import *
 import csv
 import random
 
+wielkosc_populacji = 100
+liczba_pokolen = 100
+
+liczba_krzyzowan = 50
+rozmiar_turnieju = 2
+prawdopodobienstwo_mutacji = 0.1
+liczba_elit = 10
+
 klienci = []
 oznaczenia_klientow = []
+
+macierz_kosztow = Koszt.wczytaj_macierz_kosztow('daneKoszt.csv')
 
 with open('daneKlient.csv', newline='', encoding='utf-8') as plik_csv:
     czytnik_csv = csv.reader(plik_csv)
@@ -16,13 +26,42 @@ with open('daneKlient.csv', newline='', encoding='utf-8') as plik_csv:
 
         oznaczenia_klientow.append(nowy_klient.oznaczenie)
 
-liczba_klientow = len(klienci)
-macierz_kosztow = Koszt.wczytaj_macierz_kosztow('daneKoszt.csv')
-wielkosc_populacji = liczba_klientow ** 2
+populacja = generuj_populacje(oznaczenia_klientow, wielkosc_populacji)
 
-def kodowanie_genotypu():
-    random.shuffle(oznaczenia_klientow)
-    return oznaczenia_klientow.copy()
+# Ewolucja populacji przez określoną liczbę pokoleń
 
-poczatkowa_populacja = AlgorytmGenetyczny.kodowanie_genotypu(oznaczenia_klientow)
 
+for pokolenie in range(liczba_pokolen):
+    # Ocena jakości każdego chromosomu w populacji
+    wyniki_oceny = [Koszt.sumuj_koszt(trasa, macierz_kosztow) for trasa in populacja]
+
+    # Selekcja turniejowa
+    nowa_populacja = selekcja_turniejowa(populacja, wyniki_oceny, rozmiar_turnieju)
+
+    # Krzyżowanie jednopunktowe
+    for _ in range(liczba_krzyzowan):
+        indeks_rodzica1, indeks_rodzica2 = random.sample(range(len(nowa_populacja)), 2)
+        rodzic1, rodzic2 = nowa_populacja[indeks_rodzica1], nowa_populacja[indeks_rodzica2]
+        dziecko1, dziecko2 = krzyzowanie_jednopunktowe(rodzic1, rodzic2)
+        nowa_populacja.extend([dziecko1, dziecko2])
+
+    # Mutacja zmiany miast
+    for i in range(len(nowa_populacja)):
+        nowa_populacja[i] = mutacja_zmiany_miast(nowa_populacja[i], prawdopodobienstwo_mutacji)
+
+    # Ocena jakości nowej populacji
+    wyniki_oceny_nowej_populacji = [Koszt.sumuj_koszt(trasa, macierz_kosztow) for trasa in nowa_populacja]
+
+    # Elitaryzm - wybór najlepszych rozwiązań do kolejnego pokolenia
+    najlepsze_indeksy = sorted(range(len(wyniki_oceny_nowej_populacji)), key=lambda k: wyniki_oceny_nowej_populacji[k])[
+                        :liczba_elit]
+    nowa_populacja = [nowa_populacja[indeks] for indeks in najlepsze_indeksy]
+
+    # Zastąpienie starej populacji nową populacją
+    populacja = nowa_populacja
+
+# Ostateczna ocena jakości najlepszego rozwiązania
+wyniki_oceny = [Koszt.sumuj_koszt(trasa, macierz_kosztow) for trasa in populacja]
+najlepsze_rozwiazanie = populacja[wyniki_oceny.index(min(wyniki_oceny))]
+
+print(najlepsze_rozwiazanie)
