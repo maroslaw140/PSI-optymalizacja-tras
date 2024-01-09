22c1512223856b085dedff1c6a473b860842d85a
maroslaw140 <marek.muller911@gmail.com>
2024-01-09 21:49:40 +0100

    Przygotowanie danych Klient

--- classKlient.py
+++ classKlient.py
@@ -1,9 +1,15 @@
 class Klient:
-    oznaczenie = ""
-    miasto = ""
+    def __init__(self):
+        self.oznaczenie = ""
+        self.miasto = ""
+        self.wspolrzednaX = 0.0
+        self.wspolrzednaY = 0.0
 
-    wspolrzednaX  = 0.0
-    wspolrzednaY = 0.0
-
-    def __init__(self, oznaczenie):
-        self.oznaczenie = oznaczenie
+    def przypisz(self, linia):
+        dane = linia.strip().split(',')
+        if len(dane) == 4:
+            self.oznaczenie, self.miasto, wspolrzednaX, wspolrzednaY = dane
+            self.wspolrzednaX = float(wspolrzednaX)
+            self.wspolrzednaY = float(wspolrzednaY)
+        else:
+            raise ValueError("Nieprawidłowy format danych")
