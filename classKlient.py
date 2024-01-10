13f801e655d66e761471a5d2c127fd20a662ff29
maroslaw140 <marek.muller911@gmail.com>
2024-01-10 02:54:29 +0100

    Obsługa błędów

--- classKlient.py
+++ classKlient.py
@@ -5,11 +5,15 @@ class Klient:
         self.wspolrzednaX = 0.0
         self.wspolrzednaY = 0.0
 
+
     def przypisz(self, linia):
         dane = linia.strip().split(',')
         if len(dane) == 4:
             self.oznaczenie, self.miasto, wspolrzednaX, wspolrzednaY = dane
-            self.wspolrzednaX = float(wspolrzednaX)
-            self.wspolrzednaY = float(wspolrzednaY)
+            try:
+                self.wspolrzednaX = float(wspolrzednaX)
+                self.wspolrzednaY = float(wspolrzednaY)
+            except ValueError:
+                raise ValueError("Nieprawidłowy format współrzędnych")
         else:
             raise ValueError("Nieprawidłowy format danych")
--- koszt.py
+++ koszt.py
@@ -1,27 +1,41 @@
 def wczytaj_macierz_kosztow(plik):
     macierz_kosztow = {}
 
-    with open(plik, newline='') as plik_csv:
-        import csv
-        czytnik_csv = csv.reader(plik_csv)
-        nazwy_klientow = None
-        for i, linia in enumerate(czytnik_csv):
-            if i == 0:  # Pierwszy wiersz zawiera nazwy klientów
-                nazwy_klientow = linia[1:]
-                for klient in nazwy_klientow:
-                    macierz_kosztow[klient] = {}
-            else:
-                aktualny_klient = linia[0]
-                koszty = linia[1:]
-                for j, koszt in enumerate(koszty):
-                    macierz_kosztow[aktualny_klient][nazwy_klientow[j]] = int(koszt)
+    try:
+        with open(plik, newline='') as plik_csv:
+            import csv
+            czytnik_csv = csv.reader(plik_csv)
+            nazwy_klientow = None
+            for i, linia in enumerate(czytnik_csv):
+                if i == 0:
+                    nazwy_klientow = linia[1:]
+                    for klient in nazwy_klientow:
+                        macierz_kosztow[klient] = {}
+                else:
+                    klient_aktualny = linia[0]
+                    koszty = linia[1:]
+                    for j, koszt in enumerate(koszty):
+                        macierz_kosztow[klient_aktualny][nazwy_klientow[j]] = int(koszt)
+
+    except FileNotFoundError as e:
+        print(f"Błąd odczytu pliku: {e}")
+    except Exception as e:
+        print(f"Błąd: {e}")
 
     return macierz_kosztow
 
+
 def sumuj_koszt(trasa, macierz_kosztow):
     koszt = 0
-    for i in range(len(trasa) - 1):
-        klient_biezacy = trasa[i]
-        klient_nastepny = trasa[i + 1]
-        koszt += macierz_kosztow[klient_biezacy][klient_nastepny]
+    try:
+        for i in range(len(trasa) - 1):
+            klient_aktualny = trasa[i]
+            klient_nastepny = trasa[i + 1]
+            koszt += macierz_kosztow[klient_aktualny][klient_nastepny]
+
+    except KeyError as e:
+        print(f"Błąd: brak klucza {e} w macierzy kosztów!")
+    except Exception as e:
+        print(f"Błąd: {e}")
+
     return koszt
